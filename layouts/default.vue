304eaae54f4a03cc273949c724720b55aa957bb2
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-01-02 13:27:34 -0600

    feat: agregar color y quitar data innecesario

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,12 @@
 <template>
   <v-app dark>
-    <v-main>
+    <v-main class="bg-main">
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer :absolute="!fixed" app>
+    <v-footer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
-
-<script>
-export default {
-  name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
-  },
-}
-</script>
