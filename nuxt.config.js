5c1c0d994b9abbf76d8ec3e7ca56ec5bba9bac9b
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-01-02 13:02:33 -0600

    feat: crear global styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     titleTemplate: '%s - Gestor-Tareas',
     title: 'Gestor-Tareas',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'es',
     },
     meta: [
       { charset: 'utf-8' },
@@ -18,7 +18,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~/assets/global-styles.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
