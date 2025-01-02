451944083950c6e92b1d0bb8d061c79be4227e9b
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-01-02 13:27:53 -0600

    feat: cambiar color principal

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: "#3A3B73",
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
